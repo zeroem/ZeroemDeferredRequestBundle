67ded2b9f26e2a7e119c2edbe25ad3ff03dd3eb0
Darrell Hamilton <darrell.noice@gmail.com>
2012-06-21 20:21:10 -0700

    set up the annotation loader for unit testing

---
+++ Tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+$loader = require_once(__DIR__.'/../vendor/autoload.php');
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+AnnotationRegistry::registerLoader(array($loader,"loadClass"));
