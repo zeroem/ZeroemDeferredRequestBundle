c1a042c228b1f7659990ef13c3fc564c163168f5
Darrell Hamilton <darrellh@4over.com>
2012-06-22 15:10:42 -0700

    implement the defer controller as a service

--- Controller/DeferController.php
+++ Controller/DeferController.php
@@ -3,18 +3,29 @@
 namespace Zeroem\DeferredRequestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request as HttpRequest;
 
 use Zeroem\DeferredRequestBundle\Entity\Request;
+use Zeroem\DeferredRequestBundle\DeferResponseBuilder;
 
-class DeferController extends Controller
+class DeferController
 {
-  public function deferRequest() {
-    $request = new Request();
-    $request->setRequest($this->getRequest());
 
-    $em = $this->getDoctrine()->getEntityManager();
-    $em->persist($request);
-    $em->flush();
+  private $entityManager;
+  private $responseBuilder;
+
+  public function __construct($entityManager, DeferResponseBuilder $responseBuilder) {
+    $this->entityManager = $entityManager;
+    $this->responseBuilder = $responseBuilder;
+  }
+
+  public function deferRequest(HttpRequest $httpRequest) {
+    $request = new Request();
+    $request->setRequest($httpRequest);
+
+    $this->entityManager->persist($request);
+    $this->entityManager->flush();
+
+    return $this->responseBuilder->buildResponse($request);
   }
 }
