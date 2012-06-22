7a7c4cda1deffdebb5f236b72cef2b0d412e8e3c
Darrell Hamilton <darrellh@4over.com>
2012-06-22 15:09:56 -0700

    add required 'Action' suffix and return a response

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -4,13 +4,14 @@ namespace Zeroem\DeferredRequestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 
 class ApiController extends Controller
 {
-  public function deferredResponse($id)
+  public function deferredResponseAction($id)
   {
     $entity = $this->getRequestEntity($id);
 
@@ -21,14 +22,14 @@ class ApiController extends Controller
     throw new NotFoundHttpException('Deferred Request does not have a response.');
   }
 
-  public function deferredRequestStatus($id) {
+  public function deferredRequestStatusAction($id) {
     $entity = $this->getRequestEntity($id);
 
     if($entity->getResponse()) {
-      return "done";
+      return new Response("done");
     }
 
-    return "not done";
+    return new Response("not done");
   }
 
   private function getRequestEntity($id) {
