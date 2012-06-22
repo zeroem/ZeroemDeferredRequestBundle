312cb33bcd60ca9e8548b1ec364e0698e2b91d79
Darrell Hamilton <darrell.noice@gmail.com>
2012-06-21 22:29:33 -0700

    additional autoloader for loading annotations outside of a standard symfony install

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,4 +4,14 @@ $loader = require_once(__DIR__.'/../vendor/autoload.php');
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-AnnotationRegistry::registerLoader(array($loader,"loadClass"));
+//spl_autoload_register("derpAutoload");
+AnnotationRegistry::registerLoader(
+  function ($class) {
+    if (0 === strpos(ltrim($class, '/'), 'Zeroem\DeferredRequestBundle')) {
+      if (file_exists($file = __DIR__.'/../'.substr(str_replace('\\', '/', $class), strlen('Zeroem\DeferredRequestBundle')).'.php')) {
+        require_once $file;
+        return true;
+      }
+    }
+  }
+);
